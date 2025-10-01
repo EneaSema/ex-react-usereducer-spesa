ad120ebb94098a8cb7c9a63332527fab7bfab92c
EneaSema <sema.enea1993@gmail.com>
2025-10-01 15:58:41 +0200

    resolve milestone 2 guardato soluzione di Hyur per  funzione addTocart che non mi tornava

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+// importo useState
+
+import { useState } from "react";
+
 function App() {
   // array di oggetti fornito dalla traccia
   const products = [
@@ -7,6 +11,22 @@ function App() {
     { name: "Pasta", price: 0.7 },
   ];
 
+  //  creo stato locale addedProducts ( inizialmente array vuoto), devo importare useState
+
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  function addTocart(p) {
+    const isProductAlreadyAdded = addedProducts.some(
+      (ap) => ap.name === p.name
+    );
+    if (isProductAlreadyAdded) {
+      return;
+    }
+
+    setAddedProducts([...addedProducts, { ...p, quantity: 1 }]);
+  }
+  console.log(addedProducts);
+
   return (
     <>
       <h1>EX - Carrello della spesa</h1>
@@ -14,10 +34,25 @@ function App() {
       <ul>
         {products.map((p, i) => (
           <li key={i}>
-            {p.name} {p.price.toFixed(2)} €
+            {p.name} {p.price.toFixed(2)} €{" "}
+            <button onClick={() => addTocart(p)}>Aggiungi al Carrello</button>
           </li>
         ))}
       </ul>
+
+      {addedProducts.length > 0 && (
+        <>
+          <h2>Lista prdotti nel Carrello</h2>
+          <ul>
+            {addedProducts.map((ap, i) => (
+              <li>
+                {ap.name} {ap.price.toFixed(2)} € {""} quantity:{``}
+                {ap.quantity}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
