1fc071b84c79be3008b25686ae29372291c71952
EneaSema <sema.enea1993@gmail.com>
2025-10-07 13:43:39 +0200

    resolve milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 export default function App() {
+  // milestone 1
+  // array fornito
+  const products = [
+    { name: "Mela", price: 0.5 },
+    { name: "Pane", price: 1.2 },
+    { name: "Latte", price: 1.0 },
+    { name: "Pasta", price: 0.7 },
+  ];
+
   return (
     <>
       <h1>EX - Carrello della spesa</h1>
+      <ul>
+        {products.map((p, i) => {
+          return (
+            <li key={i}>
+              <span>
+                {p.name} {p.price.toFixed(2)} €
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
